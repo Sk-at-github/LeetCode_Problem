73967edb61c5de2f65b039aff091ec5fa6a178c4
Sk-at-github <98371965+Sk-at-github@users.noreply.github.com>
2022-11-10 00:34:44 +0530

    Create countprime_seive_of_eratosthenes.cpp
    1st all prime 
    then check all the multiples as non prime

---
+++ countprime_seive_of_eratosthenes.cpp
@@ -0,0 +1,27 @@
+
+//leetcode- https://leetcode.com/problems/count-primes/submissions/
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+    
+        int cnt = 0;
+        vector<bool> prime(n+1,true);
+        prime[0]=prime[1]= false;
+        
+        for(int i = 0;i< n; i++ ){
+        if(prime[i]){
+            cnt++;
+            for(int j = 2*i;j<n;j=j+i){
+                prime[j] = 0;
+            }
+        }
+        }
+    
+        
+        
+        
+       return cnt; 
+    }
+};
